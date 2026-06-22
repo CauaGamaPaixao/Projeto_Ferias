d922bbf4bfe6f38d292b74b3fdecc0e0a7586638
CauaGamaPaixao <caua.paixao@sptech.school>
2026-06-22 19:28:01 -0300

    Add ProductService default constructor compatibility

--- Code/src/main/java/br/com/projetoferias/service/ProductService.java
+++ Code/src/main/java/br/com/projetoferias/service/ProductService.java
@@ -2,7 +2,9 @@ package br.com.projetoferias.service;
 
 import br.com.projetoferias.model.Product;
 import br.com.projetoferias.model.ProductCategory;
+import br.com.projetoferias.repository.InMemoryProductRepository;
 import br.com.projetoferias.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +16,11 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    public ProductService() {
+        this(new InMemoryProductRepository());
+    }
+
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
